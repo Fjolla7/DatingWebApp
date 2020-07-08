f3ba1721683bd7b4fe1c44ca36f713dd9ee1c37f
Fjolla7 <fjolla.ramadanii@outlook.com>
2020-07-08 15:58:31 +0200

    End of section 14

--- DatingWebApp-SPA/src/app/app.module.ts
+++ DatingWebApp-SPA/src/app/app.module.ts
@@ -5,7 +5,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule} from '@kolkov/ngx-gallery';
@@ -59,8 +61,10 @@ export function tokenGetter() {
       ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       BsDatepickerModule.forRoot(),
+      PaginationModule.forRoot(),
       TabsModule.forRoot(),
-      RouterModule.forRoot(appRoutes),      
+      ButtonsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
       NgxGalleryModule,
       FileUploadModule,
       JwtModule.forRoot({
